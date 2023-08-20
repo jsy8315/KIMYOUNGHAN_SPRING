861ed449483d4f902d7beb8cf980e904f3fe6684
Suyoung Jung <tdhitbtd@gmail.com>
2023-08-20 20:28:33 +0900

    섹션02 - API

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 //controller는 제일 먼저 거쳐감
@@ -13,6 +14,7 @@ public class HelloController {
         model.addAttribute("data", "attribute의 value인 hello!");
         return "hello"; //hello.html으로 가서 렌더링하셈
     }
+
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name, Model model) {
         //"localhost:8080/hello=mvc?name=꾹이"의 입력에서, name은 "꾹이"로 넘어간다
@@ -23,5 +25,33 @@ public class HelloController {
         //여기의 name도 꾹이로 바뀜, 그리고 모델에 담김
         return "hello-template";
         //hello-template로 넘어간다
+
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    //html에 나오는 body가 아님
+    //http의 body에 데이터를 직접 넣어주겠다란 의미
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+        //""hello " + name"이 그대로, 데이터를 내려줌
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody //http의 응답에 데이터를 그대로 넘겨야겠따!
+    public Hello HelloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
+
