dae082580c52e81fff6f8877635bed4b2a7a5dd1
Suyoung Jung <tdhitbtd@gmail.com>
2023-08-30 20:23:25 +0900

    스프링MVC1편 섹션07-프로젝트 생성

--- hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -8,14 +8,14 @@ import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
-    private final EntityManager em;
+
     @Autowired
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository() = memberRepository;
     }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
     @Bean
     public MemberRepository memberRepository() {
